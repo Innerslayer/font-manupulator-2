78a3742699a6139e5c27f9df50af0ada4028be9e
Innerslayer <103618774+Innerslayer@users.noreply.github.com>
2022-11-26 06:29:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
+}
+
+function draw() {
+    background("grey");
+    
+}
+
+function modelloaded() {
+    console.log("poseNet is initialized");
+
+}
+
+function gotposes(results) {
+    if(results.length>0) {
+        console.log(results); 
+    }
+
+    
+}
